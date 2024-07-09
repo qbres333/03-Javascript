887011c84ca72719f359f2b5c92a806ad2e0bd1b
Fin M <qbres333@hotmail.com>
2024-07-08 22:32:00 -0400

    day 1 activities

--- 01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,9 @@
 // Write Your JavaScript Code Here
+let personName = "Finola";
+let pets = "0";
+let funFact = "I love winter weather.";
+
+console.log('My name is', personName);
+console.log('I have', pets, 'pets.');
+console.log('Fun fact:', funFact);
+
