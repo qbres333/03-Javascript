9a8dc00ec258203ac5eb4d0bf85cb17260f84bb3
Fin M <qbres333@hotmail.com>
2024-07-09 00:11:50 -0400

    day 1 JS

--- 01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
+++ 01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
@@ -1,6 +1,20 @@
 // Change the values and operators below to test your algorithm meets all conditions
 const x = 50;
-const expression1 = (x < 25);
-const expression2 = (x > 50);
+const expression1 = (x <= 50);
+const expression2 = (x >= 50);
 
 // Write Your JavaScript Code Here
+if(expression1 && expression2) {
+    console.log(expression1, '\u2705', expression2, '\u2705')
+}
+else if(expression1 && !expression2) {
+    console.log(expression1, '\u2705', expression2, '\u274C')
+}
+else if(!expression1 && expression2) {
+    console.log(expression1, '\u274C', expression2, '\u2705')
+}
+else {
+    console.log(expression1, '\u274C', expression2, '\u274C')
+}
+
+// \u274C is cross mark
