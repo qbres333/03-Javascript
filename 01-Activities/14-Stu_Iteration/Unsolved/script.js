5892983143b8a13c21dee09518d7413a69007736
Fin M <qbres333@hotmail.com>
2024-07-10 23:12:48 -0400

    day 2 JS

--- 01-Activities/14-Stu_Iteration/Unsolved/script.js
+++ 01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -10,3 +10,21 @@ for(let i = 0; i < students.length; i++) {
 for(const name of students) {
     console.log(`Great to see you, ${name}!`);
 }
+
+let result = "";
+let j = 0;
+
+// do {
+//     j = j+1;
+//     result = result + j;
+// } while (j < students.length);
+/**this do-while loop runs when j = 5. Why? 
+ * Because the code block is executed before the 
+ * condition is tested. The code will always run at least once. */
+
+// console.log(`Great to see you, ${students[j]}!`)
+
+while(j < students.length) {
+    console.log(`Great to see you, ${students[j]}!`);
+    j++;
+}
