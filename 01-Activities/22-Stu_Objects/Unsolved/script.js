ecb857e3a7fba7ab2e74ae82459e904ad2ebcafd
Fin M <qbres333@hotmail.com>
2024-07-11 22:14:53 -0400

    day 3 JS

--- 01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,17 @@
 // WRITE YOUR CODE BELOW
+const customerDrink =  {
+    drinkName: "milk",
+    numSugars: 3,
+    orderReady: true
+
+}
+
+console.log(customerDrink);
+
+if(customerDrink.orderReady = true) {
+    console.log(`Ready for pick-up`)
+}
+else {
+    console.log(`Still in order queue`)
+}
+
