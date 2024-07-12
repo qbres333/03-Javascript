54c521db1068cb166edc521c34f60af77079aeb1
Fin M <qbres333@hotmail.com>
2024-07-12 01:08:56 -0400

    RPS game

--- 01-Activities/28-Stu_Mini-Project/script.js
+++ 01-Activities/28-Stu_Mini-Project/script.js
@@ -1,33 +1,48 @@
 
 // all possible choices for the game
 const choices = ["r","p","s"];
+//create object that tracks wins and losses
 
-
-function startGame {
+function startGame() {
 
 //shows a window with the welcome message
-window.alert(`Welcome to Rock, Paper, Scissors!`)
+    window.alert(`Welcome to Rock, Paper, Scissors!`)
 
 // there are 2 users, player and computer
 // player inputs their choice
-const userChoice = prompt('Choose between rock (R, r), paper (P, p) or scissors (S, s)');
+    const userChoice = prompt('Choose between rock (R, r), paper (P, p) or scissors (S, s)');
 //store valid user choice
-const userValid = choices.includes(userChoice.toLowerCase());
+    const userValid = choices.includes(userChoice.toLowerCase());
 //if the choice is invalid, prompt for a new input
 
-if(userValid) {    //returns true or false
+    if(userValid) {    //returns true or false
     // if true, computer picks a random element of the array
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    const computer = choices[randomIndex];
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        const computer = choices[randomIndex];
     
 // how do we know who wins?
+        if(userChoice == computer) {
+            alert(`It's a tie! No one wins.`);
+            confirm(`Would you like to play again?`);
+        } 
+        // R beats S
+        // P beats R
+        // S beats P
+        else if( (userChoice.toLowerCase == "r" && computer == "s") || (userChoice.toLowerCase == "p" && computer == "r") || (userChoice.toLowerCase == "s" && computer == "p")) {
+            alert(`You beat the computer!`);
+            confirm(`Would you like to play again?`);
+        }
+        else {
+            alert(`The computer wins!`);
+            confirm(`Would you like to play again?`);
+        }
+            
+    } else {
+        alert(`You made a boo boo. Please try again.`);
+        location.reload();
+    }
 
-} else {
-    alert(`You made a boo boo. Please try again.`)
-    location.reload();
-}
-
-
+return;
 }
 
 startGame();
