4af5554a0474b68c4e5dca4904bd42d837a4f815
Fin M <qbres333@hotmail.com>
2024-07-12 00:41:53 -0400

    RPS game

--- 01-Activities/28-Stu_Mini-Project/script.js
+++ 01-Activities/28-Stu_Mini-Project/script.js
@@ -1,14 +1,32 @@
 
 // all possible choices for the game
-const choose = ["R","r","P","p","S","s"];
+const choices = ["r","p","s"];
 
-// there are 2 users, player and computer
-// how do we know who wins?
-// player inputs their choice
-    //if the choice is invalid, prompt for a new input
-// computer picks a random element of the array
+
+function startGame {
 
 //shows a window with the welcome message
 window.alert(`Welcome to Rock, Paper, Scissors!`)
 
+// there are 2 users, player and computer
+// player inputs their choice
+const userChoice = prompt('Choose between rock (R, r), paper (P, p) or scissors (S, s)');
+//store valid user choice
+const userValid = choices.includes(userChoice.toLowerCase());
+//if the choice is invalid, prompt for a new input
 
+if(userValid) {    //returns true or false
+    // if true, computer picks a random element of the array
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    const computer = choices[randomIndex];
+    
+// how do we know who wins?
+
+} else {
+    location.reload();
+}
+
+
+}
+
+startGame();
